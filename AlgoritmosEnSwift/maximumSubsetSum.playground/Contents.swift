754e592f5dc45c7645fcc0636f9fdb5b51032314
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2021-01-05 14:50:15 +0100

    add maximum non adjecent subset

--- AlgoritmosEnSwift/maximumSubsetSum.playground/Contents.swift
+++ AlgoritmosEnSwift/maximumSubsetSum.playground/Contents.swift
@@ -1,17 +1,48 @@
 import UIKit
 
+//maximumSubsetSum ( NON ADJACENT )
 class Program {
-  func maximumSubsetSum(array: [Int]) -> Int {
 
-        maxSoFar
+    // Space O(1)  -- Time O(N)
+    func maximumSubsetSum(array: [Int]) -> Int {
+
+        var maxSums = [Int]()
         guard !array.isEmpty else { return 0 }
 
-        for index in stride(from: 0, through: array.count - 1, by: 2) {
-            print(array[index])
+        for index in stride(from: 0, through: array.count - 1, by: 1) {
+            if index == 0 {
+                maxSums.append(array[0])
+            } else if index == 1 {
+                maxSums.append(max(array[0], array[1]))
+            } else {
+                let maxSum = max(maxSums[index-1], maxSums[index-2] + array[index])
+                maxSums.append(maxSum)
+            }
         }
-
-        return 1
-  }
+        return maxSums[array.count-1]
+    }
 }
 
-Program().maximumSubsetSum(array: [75, 105, 120, 75, 90, 135])
+Program().maximumSubsetSum(array: [7, 10, 12, 7, 9, 14])
+
+
+// Space O(1)  -- Time O(1)
+class BetterProgram {
+
+    func maximumSubsetSum(array: [Int]) -> Int {
+        guard !array.isEmpty else { return 0 }
+        guard array.count > 1 else { return array[0] }
+        var second = array[0]
+        var first = max(second, array[1])
+
+        guard !array.isEmpty else { return 0 }
+        for index in stride(from: 2, through: array.count - 1, by: 1) {
+            let maxSum = max(first, second + array[index])
+            second = first
+            first = maxSum
+        }
+        return first
+    }
+}
+
+BetterProgram().maximumSubsetSum(array: [7, 10, 12, 7, 9, 14])
